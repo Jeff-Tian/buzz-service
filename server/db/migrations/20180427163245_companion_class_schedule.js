f714dd2b48b0ec8457b5d241ceade197e6ed8a70
Jeff Tian <Jeff.Tian@outlook.com>
2018-04-27 17:48:06 +0800

    Drop foreign first

--- server/db/migrations/20180427163245_companion_class_schedule.js
+++ server/db/migrations/20180427163245_companion_class_schedule.js
@@ -1,13 +1,25 @@
 exports.up = function (knex, Promise) {
     return knex.schema.table('companion_class_schedule', table => {
+        if (process.env.NODE_ENV !== 'test') {
+            table.dropForeign('user_id')
+        }
         table.dropUnique(['user_id', 'batch_id', 'start_time'])
         table.unique(['user_id', 'start_time', 'status'])
+        if (process.env.NODE_ENV !== 'test') {
+            table.foreign('user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
+        }
     })
 }
 
 exports.down = function (knex, Promise) {
     return knex.schema.table('companion_class_schedule', table => {
+        if (process.env.NODE_ENV !== 'test') {
+            table.dropForeign('user_id')
+        }
         table.dropUnique(['user_id', 'start_time', 'status'])
         table.unique(['user_id', 'batch_id', 'start_time'])
+        if (process.env.NODE_ENV !== 'test') {
+            table.foreign('user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
+        }
     })
 }
