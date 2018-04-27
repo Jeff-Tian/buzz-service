1ab78bd3aaca907adc25781ef73c5949a04b02b6
Jeff Tian <Jeff.Tian@outlook.com>
2018-04-27 17:56:10 +0800

    fix typo

--- server/db/migrations/20180427163231_student_class_schedule.js
+++ server/db/migrations/20180427163231_student_class_schedule.js
@@ -3,7 +3,7 @@ const t = require('knex/lib/dialects/mysql/schema/tablecompiler')
 exports.up = function (knex, Promise) {
     return knex.schema.table('student_class_schedule', table => {
         if (process.env.NODE_ENV !== 'test') {
-            table.foreign('user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
+            table.dropForeign('user_id')
         }
         table.dropUnique(['user_id', 'batch_id', 'start_time'])
         table.unique(['user_id', 'start_time', 'status'])
@@ -16,7 +16,7 @@ exports.up = function (knex, Promise) {
 exports.down = function (knex, Promise) {
     return knex.schema.table('student_class_schedule', table => {
         if (process.env.NODE_ENV !== 'test') {
-            table.foreign('user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
+            table.dropForeign('user_id')
         }
         table.dropUnique(['user_id', 'start_time', 'status'])
         table.unique(['user_id', 'batch_id', 'start_time'])
