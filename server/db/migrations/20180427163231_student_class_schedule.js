ae2c9d492a28caee4552eaa037bc0dc9e8dbfb91
Jeff Tian <Jeff.Tian@outlook.com>
2018-04-27 18:10:36 +0800

    qa student_class_schedul unique already deleted

--- server/db/migrations/20180427163231_student_class_schedule.js
+++ server/db/migrations/20180427163231_student_class_schedule.js
@@ -4,8 +4,8 @@ exports.up = function (knex, Promise) {
     return knex.schema.table('student_class_schedule', table => {
         if (process.env.NODE_ENV !== 'test' && process.env.NODE_ENV !== 'qa') {
             table.dropForeign('user_id')
+            table.dropUnique(['user_id', 'batch_id', 'start_time'])
         }
-        table.dropUnique(['user_id', 'batch_id', 'start_time'])
         table.unique(['user_id', 'start_time', 'status'])
         if (process.env.NODE_ENV !== 'test') {
             table.foreign('user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
