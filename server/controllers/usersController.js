84f89603300d293544012d6b2a5312a0a4989eeb
Jeff Tian <Jeff.Tian@outlook.com>
2018-05-10 10:12:02 +0800

    remove redundant parenthesis

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -51,7 +51,7 @@ function selectUsers(isContextSecure) {
 
 function filterByTime(search, start_time = new Date(1900, 1, 1), end_time = new Date(2100, 1, 1)) {
     return search
-        .andWhereRaw('users.user_id in ((select distinct user_id from student_class_schedule where student_class_schedule.start_time >= ? and student_class_schedule.end_time < ?) union all (select distinct user_id from companion_class_schedule where companion_class_schedule.start_time >= ? and companion_class_schedule.end_time < ?))', [start_time, end_time, start_time, end_time])
+        .andWhereRaw('users.user_id in (select distinct user_id from student_class_schedule where student_class_schedule.start_time >= ? and student_class_schedule.end_time < ? union all select distinct user_id from companion_class_schedule where companion_class_schedule.start_time >= ? and companion_class_schedule.end_time < ?)', [start_time, end_time, start_time, end_time])
 }
 
 const search = async ctx => {
