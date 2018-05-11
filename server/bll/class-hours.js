4c2dc7ca91517116048fcc824cf14077031bc551
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-05-11 16:42:19 +0800

    [!] 扣除正数课时且总课时<=2时发续费通知

--- server/bll/class-hours.js
+++ server/bll/class-hours.js
@@ -52,11 +52,8 @@ async function consumeClassHours(trx, userId, classHours, remark = '') {
         await trx('user_balance').insert(newClassHours)
     }
 
-    const now_currentClassHours = await getCurrentClassHours(trx, userId)
-    const now_booked_class_hours = await countBookedClasses(userId)
-    const now_all_class_hours = _.toInteger(now_currentClassHours) + _.toInteger(now_booked_class_hours)
     // TODO: Read 2 from config
-    if (now_all_class_hours < all_class_hours && all_class_hours <= 2) {
+    if ((_.toInteger(classHours) > 0) && all_class_hours <= 2) {
         await wechat.sendRenewTpl(userId, all_class_hours).catch(e => console.error('wechat:sendRenewTpl', e))
     }
 
