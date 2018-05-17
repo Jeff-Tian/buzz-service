6dde7c4913976d1381115c6004eec7686db855b5
Jeff Tian <Jeff.Tian@outlook.com>
2018-05-17 11:16:53 +0800

    Enable basic auth on qa

--- server/security/basic-auth.js
+++ server/security/basic-auth.js
@@ -3,9 +3,12 @@ const assert = require('assert')
 
 module.exports = {
     validate(ctx) {
-        // TODO: enable it after more tests
-        return true
-        // const user = auth(ctx)
-        // return (user && (user.name === process.env.BASIC_NAME && user.pass === process.env.BASIC_PASS))
+        // TODO: Delete this line after thorough test on qa
+        if (process.env.NODE_ENV === 'production') {
+            return true
+        }
+
+        const user = auth(ctx)
+        return (user && (user.name === process.env.BASIC_NAME && user.pass === process.env.BASIC_PASS))
     },
 }
