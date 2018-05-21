13e175c2515ae319491956edb12c622581926a6c
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-05-21 11:06:15 +0800

    [!] 七牛 https

--- server/common/qiniu.js
+++ server/common/qiniu.js
@@ -5,8 +5,8 @@ const config_qiniu = {
     SECRET_KEY: process.env.buzz_qiniu_secret_key,
     bucket: 'buzz-corner-user-resource',
     url: {
-        upload_url: 'http://upload.qiniu.com/',
-        resources_url: 'http://p57969ygc.bkt.clouddn.com/',
+        upload_url: 'https://upload.qiniup.com/',
+        resources_url: 'https://buzz-corner.user.resource.buzzbuzzenglish.com/',
     },
     suffix: {
         avvod: 'avvod/m3u8',
