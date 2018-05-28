9a27e8c348d3e0b0caaf7f6652eacb4b030ad46a
Jeff Tian <Jeff.Tian@outlook.com>
2018-05-28 11:17:04 +0800

    save password as plain text

--- server/security/password.js
+++ server/security/password.js
@@ -2,11 +2,13 @@ const bcrypt = require('bcryptjs')
 
 export default class Password {
     static encrypt(password) {
-        const salt = bcrypt.genSaltSync(10)
-        return bcrypt.hashSync(password, salt)
+        // const salt = bcrypt.genSaltSync(10)
+        // return bcrypt.hashSync(password, salt)
+        return password
     }
 
     static compare(passwordProvided, passwordInDb) {
-        return bcrypt.compareSync(passwordProvided, passwordInDb)
+        // return bcrypt.compareSync(passwordProvided, passwordInDb)
+        return passwordProvided === passwordInDb
     }
 }
