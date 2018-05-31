fa934c164b486456dc4e0bff0f399d50f95e58cf
Jeff Tian <Jeff.Tian@outlook.com>
2018-05-31 15:13:27 +0800

    delete redundant migrate

--- server/db/migrations/20180529150115_user_tags.js
+++ server/db/migrations/20180529150115_user_tags.js
@@ -1,10 +1,11 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('user_tags', table => {
-        table.bigInteger('user_id')
+        table.bigInteger('user_id').unsigned()
         table.string('tag')
         table.timestamp('created_at').defaultTo(knex.fn.now())
 
         table.primary(['user_id', 'tag'])
+        table.foreign('user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
     })
 }
 
