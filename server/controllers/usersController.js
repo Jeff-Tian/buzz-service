b548d7a5f536c939fff9df24fed5fe450dc73b2f
Jeff Tian <Jeff.Tian@outlook.com>
2018-06-07 18:51:57 +0800

    fix typo

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -382,13 +382,13 @@ const updateUserProfilesTable = async function (body, trx, ctx) {
     })
 
     if (basicAuth.validate(ctx)) {
-        if (body.mobile !== 'undefined' && body.mobile.indexOf('*') < 0) {
+        if (typeof body.mobile !== 'undefined' && body.mobile.indexOf('*') < 0) {
             profiles = Object.assign(profiles, makeUpdations({
                 mobile: body.mobile,
             }))
         }
 
-        if (body.email !== 'undefined' && body.email.indexOf('*') < 0) {
+        if (typeof body.email !== 'undefined' && body.email.indexOf('*') < 0) {
             profiles = Object.assign(profiles, makeUpdations({
                 email: body.email,
             }))
