68cbc70163073206801a06ebcbf57bed43e32736
Jeff Tian <Jeff.Tian@outlook.com>
2018-06-11 13:49:56 +0800

    add await

--- server/bll/user.js
+++ server/bll/user.js
@@ -133,6 +133,6 @@ module.exports = {
     },
 
     async isOfSystemUsers(userId) {
-        return !Tags.superUserExists() || Tags.containSystemUserTags((await user.getTags(userId)).map(t => t.tag))
+        return !(await Tags.superUserExists()) || Tags.containSystemUserTags((await user.getTags(userId)).map(t => t.tag))
     },
 }
