48b4ebb0bd6f2febc06496294983625c80da82c9
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-07-02 10:27:18 +0800

    [!] 入门指导课 时间

--- server/controllers/studentClassScheduleController.js
+++ server/controllers/studentClassScheduleController.js
@@ -72,7 +72,7 @@ const list = async ctx => {
         if (!_.isArray(result)) result = []
         const status = _.find(result, i => (i.classes_status === 'ended') || (i.status === 'ended')) ? 'ended' : 'confirmed'
         const CURRENT_TIMESTAMP = moment().utc().format()
-        const startTime = status === 'confirmed' ? moment().hour(0).minute(0).second(0).millisecond(0).utc().format() : moment().subtract(1, 'd').hour(0).minute(0).second(0).millisecond(0).utc().format()
+        const startTime = status === 'confirmed' ? moment().hour(10).minute(0).second(0).millisecond(0).utc().format() : moment().subtract(1, 'd').hour(10).minute(0).second(0).millisecond(0).utc().format()
         const endTime = status === 'confirmed' ? moment().hour(20).minute(0).second(0).millisecond(0).utc().format() : moment().subtract(1, 'd').hour(20).minute(0).second(0).millisecond(0).utc().format()
         result.push({
             CURRENT_TIMESTAMP: moment().utc().format(),
