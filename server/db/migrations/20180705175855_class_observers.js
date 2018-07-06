e75bac33a14979cb4ab2962365a39edb963c53b6
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-07-06 12:41:39 +0800

    [!] typo

--- server/db/migrations/20180705175855_class_observers.js
+++ server/db/migrations/20180705175855_class_observers.js
@@ -1,7 +1,7 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('class_observers', table => {
         table.bigInteger('class_id')
-        table.bigInteger('use_id')
+        table.bigInteger('user_id')
         table.timestamp('created_at').defaultTo(knex.fn.now())
         table.primary(['class_id', 'user_id'])
     })
