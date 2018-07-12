939c9210eac6f9a10719496709ab2bd0f15657cd
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-12 17:32:52 +0800

    fix migrate

--- server/db/migrations/20180712171537_class_feedback.js
+++ server/db/migrations/20180712171537_class_feedback.js
@@ -1,11 +1,13 @@
 exports.up = function (knex, Promise) {
     return knex.schema.table('class_feedback', table => {
         table.dropPrimary()
+        table.primary(['class_id', 'from_user_id', 'to_user_id', 'type'])
     })
 }
 
 exports.down = function (knex, Promise) {
     return knex.schema.table('class_feedback', table => {
-        table.primary(['class_id', 'from_user_id', 'to_user_id', 'type'])
+        table.dropPrimary()
+        table.primary(['class_id', 'from_user_id', 'to_user_id'])
     })
 }
