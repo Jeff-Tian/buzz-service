36e00f0fc28f0913acc8db6ab3e11b8a269016ae
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-07-12 17:37:13 +0800

    drop foreign and rebuild it

--- server/db/migrations/20180712171537_class_feedback.js
+++ server/db/migrations/20180712171537_class_feedback.js
@@ -1,13 +1,23 @@
 exports.up = function (knex, Promise) {
     return knex.schema.table('class_feedback', table => {
-        table.dropPrimary()
-        table.primary(['class_id', 'from_user_id', 'to_user_id', 'type'])
+        if (process.env.NODE_ENV !== 'test') {
+            table.dropForeign('class_id')
+            table.dropForeign('from_user_id')
+            table.dropForeign('to_user_id')
+            table.dropPrimary()
+            table.foreign('class_id').references('classes.class_id').onDelete('CASCADE').onUpdate('CASCADE')
+            table.foreign('from_user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
+            table.foreign('to_user_id').references('users.user_id').onDelete('CASCADE').onUpdate('CASCADE')
+            table.primary(['class_id', 'from_user_id', 'to_user_id', 'type'])
+        }
     })
 }
 
 exports.down = function (knex, Promise) {
     return knex.schema.table('class_feedback', table => {
-        table.dropPrimary()
-        table.primary(['class_id', 'from_user_id', 'to_user_id'])
+        if (process.env.NODE_ENV !== 'test') {
+            table.dropPrimary()
+            table.primary(['class_id', 'from_user_id', 'to_user_id'])
+        }
     })
 }
