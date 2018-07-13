8476e5cd21f593c47a82fc8ab5b92c841d6c7e72
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-07-13 14:34:40 +0800

    [!] longtext

--- server/db/migrations/20180713143347_faq.js
+++ server/db/migrations/20180713143347_faq.js
@@ -1,11 +1,11 @@
 exports.up = function (knex, Promise) {
     return knex.schema.table('faq', table => {
-        table.dropColumn('content')
+        table.text('content', 'longtext')
     })
 }
 
 exports.down = function (knex, Promise) {
     return knex.schema.table('faq', table => {
-        table.text('content')
+        table.dropColumn('content')
     })
 }
