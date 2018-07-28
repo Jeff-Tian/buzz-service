5bd7f1f598dbe1d02d599207395c344e261b6d55
Hsiaoyi Hsu <xreamxu@gmail.com>
2018-07-28 11:03:08 +0800

    [!] test 环境 weekly_schedule_requirements drop 之后再创建

--- server/db/migrations/20180728104435_user_profiles.js
+++ server/db/migrations/20180728104435_user_profiles.js
@@ -3,8 +3,9 @@ exports.up = function (knex, Promise) {
     if (process.env.NODE_ENV === 'test') {
         return knex.schema.table('user_profiles', table => {
             table.dropColumn('weekly_schedule_requirements')
+        }).then(() => knex.schema.table('user_profiles', table => {
             table.tinyint('weekly_schedule_requirements').defaultTo(1)
-        })
+        }))
     }
     return knex.schema.alterTable('user_profiles', table => {
         table.tinyint('weekly_schedule_requirements').defaultTo(1).alter()
@@ -15,8 +16,9 @@ exports.down = function (knex, Promise) {
     if (process.env.NODE_ENV === 'test') {
         return knex.schema.table('user_profiles', table => {
             table.dropColumn('weekly_schedule_requirements')
+        }).then(() => knex.schema.table('user_profiles', table => {
             table.tinyint('weekly_schedule_requirements')
-        })
+        }))
     }
     return knex.schema.alterTable('user_profiles', table => {
         table.tinyint('weekly_schedule_requirements').defaultTo(null).alter()
