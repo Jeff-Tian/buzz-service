1c3c720042bf55a56c9027497721185401ccd9d8
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-08-05 16:34:21 +0800

    get user id from request

--- server/controllers/classHoursController.js
+++ server/controllers/classHoursController.js
@@ -1,7 +1,7 @@
 import ClassHourHistoryBll from '../bll/class-hour-history'
 
 export default class ClassHoursController {
-    static getHistoryByUserId(userId) {
-        return ClassHourHistoryBll.getHistoryByUserId(userId)
+    static async getHistoryByUserId(ctx) {
+        return ClassHourHistoryBll.getHistoryByUserId(ctx.params.user_id)
     }
 }
