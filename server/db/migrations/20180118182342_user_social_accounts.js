6329b6af9c58c64c5d78ee7710f164301214a287
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-08-17 11:40:54 +0800

    specify string length for wechat and facebook id

--- server/db/migrations/20180118182342_user_social_accounts.js
+++ server/db/migrations/20180118182342_user_social_accounts.js
@@ -1,11 +1,11 @@
 exports.up = function (knex, Promise) {
     return knex.schema.createTable('user_social_accounts', table => {
         table.biginteger('user_id').unsigned().notNullable()
-        table.string('facebook_id')
+        table.string('facebook_id', 100)
         table.string('facebook_name')
         table.json('facebook_data')
-        table.string('wechat_openid')
-        table.string('wechat_unionid')
+        table.string('wechat_openid', 100)
+        table.string('wechat_unionid', 100)
         table.string('wechat_name')
         table.json('wechat_data')
 
