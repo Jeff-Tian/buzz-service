f6886f80e9e129b1f188912739021fec0b432189
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-08-24 14:21:10 +0800

    return json

--- server/controllers/customerFollowupController.js
+++ server/controllers/customerFollowupController.js
@@ -3,7 +3,7 @@ import CustomerFollowupBll from '../bll/customer-followup'
 export default class CustomerFollowupController {
     static async saveFollowupRecord(ctx) {
         await CustomerFollowupBll.saveFollowupRecord(ctx.params.user_id, ctx.state.user.user_id, ctx.request.body.remark)
-        ctx.body = 'done'
+        ctx.body = { done: true }
     }
 
     static async getFollowupHistory(ctx) {
