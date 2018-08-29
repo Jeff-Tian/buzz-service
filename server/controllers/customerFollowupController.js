79fc1d4764a807790f3a1baea5d820dce95f8272
Jeff Tian <jeff.tian@buzzbuzzenglish.com>
2018-08-29 15:18:57 +0800

    support pagination for customer followup

--- server/controllers/customerFollowupController.js
+++ server/controllers/customerFollowupController.js
@@ -7,6 +7,6 @@ export default class CustomerFollowupController {
     }
 
     static async getFollowupHistory(ctx) {
-        ctx.body = await CustomerFollowupBll.getFollowupHistory(ctx.params.user_id)
+        ctx.body = await CustomerFollowupBll.getFollowupHistory(ctx.params.user_id, ctx.query.per_page, ctx.query.current_page)
     }
 }
